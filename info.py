60c184792d5017ab2c86c8e0a7cbc5aef16f29b9
awad3317 <awad3317@icloud.com>
2024-02-10 23:32:51 +0300

    Update info.py

--- info.py
+++ info.py
@@ -4,4 +4,4 @@ import requests
 from telebot.types import Message
 from telebot.types import InlineKeyboardMarkup
 from telebot.types import InlineKeyboardButton
-Token="5930953153:AAGXcHVf3HczA_8C10IbiKYp45trToZoVks"
+Token="6859697075:AAFoI2nhzsLmcnyaBMKA9vPN39SEf0PCor0"
